ce2903a45cf87038959bc614dc1a7dd31d2ac2ea
sungeun <jse4159@naver.com>
2021-11-04 22:55:40 +0900

    2739 : 구구단

---
+++ Algorithm/Baekjoon/2739/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    	int i = sc.nextInt();
+        
+        for (int j = 1; j < 10; j++) {
+            System.out.printf("%d * %d = %d\n", i, j, i * j);
+        }
+        
+    }
+}
