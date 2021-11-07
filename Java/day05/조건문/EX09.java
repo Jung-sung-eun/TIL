23c696aec6c010defd5324ab5e128321767d2f9c
sungeun <jse4159@naver.com>
2021-11-07 23:44:13 +0900

    EX09 : 영수증 출력[1단계]

---
+++ Java/day05/조건문/EX09.java
@@ -0,0 +1,43 @@
+/*
+ * # 영수증 출력[1단계]
+ * 
+ * 1. 메뉴판을 출력한다.
+ * 2. 사용자는 주문하고자 하는 메뉴의 번호를 입력한다.
+ * 3. 현금을 입력받는다.
+ * 4. 입력받은 현금과 메뉴가격을 확인해, 영수증을 출력한다.
+ * 5. 단, 현금이 부족한 경우 "현금이 부족합니다."라는 메세지를 출력한다.
+ * 
+ */
+
+import java.util.Scanner;
+public class EX09 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int price1 = 8700;
+		int price2 = 6200;
+		int price3 = 1500;
+		
+		int choice, money;
+		int charge = 0;
+		
+		System.out.println("=== 롯데리아 ===");
+		System.out.println("1.불고기 버거 : " + price1 + "원");
+		System.out.println("2.새우   버거 : " + price2 + "원");
+		System.out.println("3.콜       라 : " + price3 + "원");
+		
+		System.out.print("메뉴를 선택하세요: ");
+		choice = sc.nextInt();
+		
+		System.out.print("현금을 입력하세요: ");
+		money = sc.nextInt();
+		
+		if (choice == 1) { charge = money - price1; }
+		else if(choice == 2) { charge = money - price2; }
+		else if(choice == 3) { charge = money - price3; }
+		
+		// 한번에 처리
+		if (charge >= 0) { System.out.printf("잔돈은 %d원 입니다", charge); }
+		else { System.out.println("현금이 부족합니다"); }
+	}
+}
