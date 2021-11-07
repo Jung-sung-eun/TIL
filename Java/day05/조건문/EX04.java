948fa4847b3f8978d9b1d6707892ac69fd4daec9
sungeun <jse4159@naver.com>
2021-11-07 23:12:26 +0900

    EX04 : 로그인[2단계]

---
+++ Java/day05/조건문/EX04.java
@@ -0,0 +1,38 @@
+/*
+ * # 로그인[2단계]
+ * 
+ * 1. Id와 Pw를 입력받아 회원가입을 진행한다.
+ * 2. 이후 로그인을 위해 다시 Id와 Pw를 입력받는다.
+ * 3. 가입 후 저장된 데이터와 로그인 시 입력받은 데이터를 비교한다.
+ * 예) 로그인 성공 or 로그인 실패
+ */
+
+import java.util.Scanner;
+public class EX04 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int dbId, dbPw;
+		int myId, myPw;
+		
+		System.out.println("==== 회원가입 ====");
+		System.out.print("가입할 ID 입력: ");
+		dbId = sc.nextInt();
+		
+		System.out.print("가입할 PW 입력: ");
+		dbPw = sc.nextInt();
+		
+		System.out.println("==== 로그인 ====");
+		System.out.print("ID 입력: ");
+		myId = sc.nextInt();
+		
+		System.out.print("PW 입력: ");
+		myPw = sc.nextInt();
+		
+		if( dbId == myId && dbPw == myPw ) {
+        System.out.println("로그인 성공");
+        } else {
+        System.out.println("로그인 실패");
+        }
+	}
+}
